f8cdd17ebe4435e6083dd415ddb3b642f9c9b25e
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-16 19:54:11 -0700

    price-quote-assignment

---
+++ Basic-C#-Programs/Price-QuoteApplicationAssignment/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_QuoteApplicationAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Enter the package weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight <= 50)
+            {
+                Console.WriteLine("Please enter the package dimensions.");
+            }
+            else
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Sorry for the inconvenience.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Width: ");
+            int Width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int dimension = height + Width + length;
+
+            if (dimension > 50)
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express. Sorry for the inconvenience.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else
+            {
+                int total = dimension * weight / 100;
+                Console.WriteLine("Your estimated total for shipping this package is: $" + total + " Thank You!");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
