6d7384d926473995b9c12dab67efc02ca10b3d11
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-16 18:33:02 -0700

    daily-report-assignment

---
+++ Basic-C#-Programs/DailyReportAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Academy of Learning Career College");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you in?");
+            string courseNumber = Console.ReadLine();
+
+            Console.WriteLine("What page number are you currently reading?");
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Do you need assistance with anything? Please answer true or false.");
+            bool input = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string experience = Console.ReadLine();
+
+            Console.WriteLine("Are there any feedback you wish like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today?");
+            int hour = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond shortly. Have a great day!");
+            Console.ReadLine();
+
+        }
+    }
+}
