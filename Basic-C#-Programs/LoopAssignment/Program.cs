c2a092c7be87b838727d61b4ab542b237a815fde
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-16 20:16:52 -0700

    loop-assignment

---
+++ Basic-C#-Programs/LoopAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            while (x < 10);
+
+        }
+    }
+}
