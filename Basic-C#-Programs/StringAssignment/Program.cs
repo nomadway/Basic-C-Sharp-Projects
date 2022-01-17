4b87a720fb33d14612ae74bfd2591b11f46f64a2
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-16 20:32:43 -0700

    StringAssignment

---
+++ Basic-C#-Programs/StringAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK-1: Concatenate 3 strings
+            string mars = "Concatenate";
+            mars += " 3";
+            mars += " strings.\n";
+            Console.WriteLine(mars);
+
+            //TASK-2: Convert String to Upper case
+            string upperword = mars.ToUpper();
+            Console.WriteLine(upperword);
+
+            //TASK-3: Create a StringBuilder and build a paragraph of text, one sentence at a time.
+            StringBuilder create = new StringBuilder();
+            create.AppendLine("Python is an awesome language to learn. ");
+            create.AppendLine("Let me show you how to use a String Builder.");
+            create.AppendLine("Did you know you could append strings on the same line of code?").AppendLine("More examples to be followed.");
+            Console.Write(create);
+
+            Console.ReadLine();
+
+        }
+    }
+}
