0979b328ab7281ddd4c519f4cf5017d4d76defa1
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-16 17:51:56 -0700

    module-8 assignment

--- Basic-C#-Programs/MathAndComparisonOperators/Program.cs
+++ Basic-C#-Programs/MathAndComparisonOperators/Program.cs
@@ -10,6 +10,68 @@ namespace MathAndComparisonOperators
     {
         static void Main(string[] args)
         {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.ReadLine();
+
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            int difference = 10 - 5;
+            Console.WriteLine("Ten minus Five = " + difference.ToString());
+            Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int quotient = 100 / 5;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            bool trueOrFalse = 12 > 5;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
+
+            int roomTemperature = 70;
+            int currentTemperature = 72;
+
+            bool isWarm = currentTemperature > roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+            
+            int roomTemperature = 70;
+            int currentTemperature = 70;
+           
+            bool isWarm = currentTemperature >= roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+            bool isWarm = currentTemperature == roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+            bool isWarm = currentTemperature != roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }
---
+++ Basic-C#-Programs/MathOperationAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperationAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task-1
+            int x = 3;
+            int y = 50;
+            Console.WriteLine(x * y);
+            Console.ReadLine();
+
+            //Task-2
+            int a = 100;
+            int b = 25;
+            Console.WriteLine(a + b);
+            Console.ReadLine();
+
+            //Task-3
+            double quotient = 100.0 / 12.5;
+            Console.WriteLine(quotient);
+            Console.ReadLine();
+
+            //Task-4
+            bool trueOrFalse = 90 > 50;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
+
+            //Task-5
+            int remainder = 15 % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+        }
+    }
+}
