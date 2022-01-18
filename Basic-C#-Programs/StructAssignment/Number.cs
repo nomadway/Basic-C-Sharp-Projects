85d6afc03d09c794c4e8f576f29076e7ab5ac38c
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-18 16:10:18 -0700

    update

---
+++ Basic-C#-Programs/StructAssignment/Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    struct Number
+    {
+        public decimal Amount { get; set; }
+
+
+
+
+
+    }
+}
