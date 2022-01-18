f60a0255bfa00c10891ccf9d6d3f874a4a066c04
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-17 23:31:04 -0700

    InheritanceAssignment

---
+++ Basic-C#-Programs/InheritanceAssignment/Person.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName() { string fullName = FirstName + " " + LastName; Console.WriteLine($"Name: {fullName}"); }
+
+    }
+}
---
+++ Basic-C#-Programs/InheritanceAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+     internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee info = new Employee();
+            info.FirstName = "Sample";
+            info.LastName = "Student";
+            info.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
