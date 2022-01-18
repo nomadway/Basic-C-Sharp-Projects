bf4e107e507db2c40b7c675a260dfdf6cf8b0a79
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-18 16:03:50 -0700

    update

---
+++ Basic-C#-Programs/EnumAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        public enum WeekDays
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main()
+        {
+
+
+            Console.WriteLine("With a capital letter write the day of week: ");
+            string entry = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(Enum.Parse(typeof(WeekDays), entry));
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("With a capital letter enter the actual day of week: ");
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
