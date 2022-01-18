adfcc4b9f056eabb4e46ef00decc4289e27bced1
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-17 22:35:33 -0700

    ClassMethod3

---
+++ Basic-C#-Programs/ClassMethod3/MathOperation.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod3
+{
+   class MathOperation
+    {
+        public void selectOne (int num1, int num2)
+        {
+            num1 = num1 + 20;
+            int Pick = num2;
+            Console.WriteLine("Second integer is " + num2);
+            return;
+        }
+    }
+}
---
+++ Basic-C#-Programs/ClassMethod3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 60;
+            int b = 70;
+
+            MathOperation n = new MathOperation();
+
+            n.selectOne(a, b);
+            n.selectOne(num2: b, num1: a);
+            Console.ReadLine();
+        }
+    }
+}
