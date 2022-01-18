e3ae12385f762270b041f8fa76451878c50e3d4c
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-18 15:55:24 -0700

    update

---
+++ Basic-C#-Programs/GenericAssignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment
+{
+    class Employee<T> : Person
+    {
+        public int Id { get; set; }
+
+        public List<T> things { get; set; }
+
+
+
+
+
+    }
+}
---
+++ Basic-C#-Programs/GenericAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> stringThings = new Employee<string>()
+            {
+                things = new List<string> { "Lexus ", "Bentley ", "Toyota" }
+            };
+            Employee<int> intThings = new Employee<int>()
+            {
+                things = new List<int> { 1, 3, 5 }
+            };
+
+
+            foreach (string item in stringThings.things)
+            {
+                Console.WriteLine(item + " ");
+            }
+            foreach (int item in intThings.things)
+            {
+                Console.WriteLine(item + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
