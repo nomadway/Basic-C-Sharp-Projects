2090a0d136fa6317be700ba452e43297deeab3b3
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-18 19:44:04 -0700

    commit

---
+++ Basic-C#-Programs/AdditionalFeaturesAssignment/ChainTwoConstructors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    class ChainTwoConstructors
+    {
+        public ChainTwoConstructors(string name) : this(name, 30)
+        {
+
+        }
+        public ChainTwoConstructors(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Basic-C#-Programs/AdditionalFeaturesAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string name = "Julia";
+            Console.WriteLine(name);
+
+            var newDict = new Dictionary<int, string>();
+
+            ChainTwoConstructors newCon = new ChainTwoConstructors("Julia");
+            Console.ReadLine();
+        }
+    }
+}
