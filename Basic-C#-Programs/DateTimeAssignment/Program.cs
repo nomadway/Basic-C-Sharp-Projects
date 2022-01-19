cdbd8426d664c5b8bae334a8fbe5f2d633adfce2
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-18 19:02:16 -0700

    commit

---
+++ Basic-C#-Programs/DateTimeAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime);
+            Console.WriteLine("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            TimeSpan plusHours = new TimeSpan(number, 0, 0);
+            DateTime nowPlusHours = dateTime.Add(plusHours);
+            Console.WriteLine(nowPlusHours);
+            Console.ReadLine();
+        }
+    }
+}
