6b9fceb4c75d8f2e0609238fc072722d2cceec40
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-18 18:57:58 -0700

    update

---
+++ Basic-C#-Programs/FileIOAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number: ");
+            string entry = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Administrator\Documents\GitHub\Basic-C-Sharp-Projects\Basic-C#-Programs\module7.txt", entry);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Administrator\Documents\GitHub\Basic-C-Sharp-Projects\Basic-C#-Programs\module7.txt"));
+            Console.ReadLine();
+        }
+    }
+}
